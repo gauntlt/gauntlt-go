54e87308f9331ec3d6a4964f1793b6ca804a45bd
James Wickett <wickett@gmail.com>
2016-02-08 20:52:12 -0600

    adding some command line parsing

--- gauntlt/main.go
+++ gauntlt/main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gauntlt/gauntlt-go/Godeps/_workspace/src/github.com/codegangsta/cli"
+	"os"
 )
 
 func main() {
 
-	fmt.Printf("the beginning")
+}
+
+func init() {
+	app := cli.NewApp()
+	app.Name = "gauntlt"
+	app.Usage = "a framework for rugged testing"
+	app.Version = "0.0.1"
+	app.ArgsUsage = "arguments"
+	app.Action = func(c *cli.Context) {
+		println("boom! I say!")
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "run",
+			Usage: "run gauntlt",
+			Action: func(c *cli.Context) {
+				println("running gauntlt: ", c.Args().First())
+			},
+		},
+	}
+	app.Run(os.Args)
+
 }
